78e343a8bdedd3edb112fd319ec7f78e5d22310f
Sk Niyaj Ali <niyaj639@gmail.com>
2024-12-28 23:35:27 +0530

    feat: Updated MainFeedScreen

--- app/src/main/java/com/niyaj/socialnetwork/presentation/main_feed/MainFeedScreen.kt
+++ app/src/main/java/com/niyaj/socialnetwork/presentation/main_feed/MainFeedScreen.kt
@@ -1,10 +1,13 @@
 package com.niyaj.socialnetwork.presentation.main_feed
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -14,6 +17,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavController
 import com.niyaj.socialnetwork.R
+import com.niyaj.socialnetwork.domain.models.Post
 import com.niyaj.socialnetwork.presentation.components.Post
 import com.niyaj.socialnetwork.presentation.components.StandardToolbar
 import com.niyaj.socialnetwork.presentation.util.Screen
@@ -22,45 +26,72 @@ import com.niyaj.socialnetwork.presentation.util.Screen
 fun MainFeedScreen(
     navController: NavController
 ) {
-    Column(
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        StandardToolbar(
-            navController = navController,
-            title = {
-                Text(
-                    text = stringResource(id = R.string.your_feed),
-                    fontWeight = FontWeight.Bold,
-                    color = MaterialTheme.colors.onBackground
-                )
-            },
-            modifier = Modifier.fillMaxWidth(),
-            showBackArrow = true,
-            navActions = {
-                IconButton(onClick = {
-                    navController.navigate(Screen.SearchScreen.route)
-                }) {
-                    Icon(
-                        imageVector = Icons.Default.Search,
-                        contentDescription = "",
-                        tint = MaterialTheme.colors.onBackground
+    Scaffold(
+        topBar = {
+            StandardToolbar(
+                navController = navController,
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.your_feed),
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colors.onBackground
                     )
+                },
+                modifier = Modifier.fillMaxWidth(),
+                showBackArrow = false,
+                navActions = {
+                    IconButton(onClick = {
+                        navController.navigate(Screen.SearchScreen.route)
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "",
+                            tint = MaterialTheme.colors.onBackground
+                        )
+                    }
                 }
+            )
+        }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+        ) {
+            item {
+                Post(
+                    post = Post(
+                        username = "Sk Niyaj Ali",
+                        imageUrl = R.drawable.image,
+                        profilePictureUrl = "",
+                        description = "this is the description of the post you are trying to create and will" +
+                                " be displayed in the profile picture when creating the post and updating the profile picture this will be displayed",
+                        likeCount = 17,
+                        commentCount = 7
+                    ),
+                    showProfileImage = false,
+                    onPostClick = {
+                        navController.navigate(Screen.PostDetailScreen.route)
+                    }
+                )
             }
-        )
-        Post(
-            post = com.niyaj.socialnetwork.domain.models.Post(
-                username = "Sk Niyaj Ali",
-                imageUrl = "",
-                profilePictureUrl = "",
-                description = "this is the description of the post you are trying to create and will" +
-                        " be displayed in the profile picture when creating the post and updating the profile picture this will be displayed",
-                likeCount = 17,
-                commentCount = 7
-            ),
-            onPostClick = {
-                navController.navigate(Screen.PostDetailScreen.route)
+            item {
+                Post(
+                    post = Post(
+                        username = "New User",
+                        imageUrl = R.drawable.kermit,
+                        profilePictureUrl = "",
+                        description = "this is the description of the post you are trying to create and will" +
+                                " be displayed in the profile picture when creating the post and updating the profile picture this will be displayed",
+                        likeCount = 17,
+                        commentCount = 7
+                    ),
+                    showProfileImage = false,
+                    onPostClick = {
+                        navController.navigate(Screen.PostDetailScreen.route)
+                    }
+                )
             }
-        )
+        }
     }
 }
